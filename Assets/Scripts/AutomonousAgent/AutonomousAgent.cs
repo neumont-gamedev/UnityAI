af86a30f0ef193459e862939553789e7b3d54b69
Raymond L Maple <rmaple@neumont.edu>
2023-01-11 21:12:48 -0700

    autonomous agent assignment

--- Assets/Scripts/AutomonousAgent/AutonomousAgent.cs
+++ Assets/Scripts/AutomonousAgent/AutonomousAgent.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class AutonomousAgent : Agent
 {
-    void Update()
-    {
-        var gameObjects = perception.GetGameObjects();
-        foreach (var gameObject in gameObjects) 
-        {
-            Debug.DrawLine(transform.position, gameObject.transform.position);
-        }
-        if (gameObjects.Length > 0)
-        {
-            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * 0);
-            movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * 1);
-        }
+	public float wanderDistance = 1;
+	public float wanderRadius = 3;
+	public float wanderDisplacement = 5;
 
-        transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
-    }
+	public float wanderAngle { get; set; } = 0;
+
+	void Update()
+	{
+		var gameObjects = perception.GetGameObjects();
+		foreach (var gameObject in gameObjects) 
+		{
+			Debug.DrawLine(transform.position, gameObject.transform.position);
+		}
+		if (gameObjects.Length > 0)
+		{
+			movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * 0);
+			movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * 1);
+		}
+
+		if (movement.acceleration.sqrMagnitude <= movement.maxForce * 0.1f)
+		{
+			movement.ApplyForce(Steering.Wander(this));
+		}
+
+		transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
+	}
 }
